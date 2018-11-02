c2c96a9b2db1b2a6dbc29bb25ec78707987ccaee
GrOOvKe <tim@groovke.be>
2018-11-02 18:27:16 +0100

    dffd

---
+++ CoreCourse.Spyshop.Domain/Kleurvariant.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreCourse.Spyshop.Domain
+{
+    public enum Kleurvariant
+    {
+        roodachtig,
+        blauwachtig,
+        groenachtig,
+
+    }
+
+   public void Test()
+    {
+
+    }
+}
+  
